03ef49f53cc9a3428461646c04c8a7e4a24ea8e0
Valeriy Manenkov <valeriy@manenkov.com>
2022-09-10 19:44:04 +0300

    Init logging

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python3
 
+from distutils.debug import DEBUG
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
+import logging
+
+logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+rootLogger = logging.getLogger()
+
+fileHandler = logging.FileHandler("{0}/{1}.log".format('target', 'solution'))
+fileHandler.setFormatter(logFormatter)
+fileHandler.setLevel(logging.NOTSET)
+rootLogger.addHandler(fileHandler)
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logFormatter)
+consoleHandler.setLevel(logging.NOTSET)
+rootLogger.addHandler(consoleHandler)
+
 
 df_issues_train = pd.read_csv("data/train_issues.csv")
 df_comment_train = pd.read_csv("data/train_comments.csv")
@@ -22,8 +38,12 @@ X_train, X_test, y_train, y_test = train_test_split(
     random_state=42,
 )
 
-y_pred = np.zeros(y_test.shape)
-print("R2 metric: " + str(r2_score(y_test, y_pred)))
+dd = merged.groupby(['project_id'])['overall_worklogs'].agg('median').reset_index()
+def getMedian(pid): 
+    return dd[dd['project_id'] == pid]['overall_worklogs'].iloc[0].astype(np.int64)
+
+y_pred = X_test[['id', 'project_id']].apply(lambda row: getMedian(row['project_id']), axis=1)
+rootLogger.warn("R2 metric: " + str(r2_score(y_test, y_pred)))
 
 df_result = pd.DataFrame()
 df_result["id"] = df_issues_test["id"]
