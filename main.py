73ead93d030b9f3503fb5aef4d630aae0d9f75ab
Valeriy Manenkov <valeriy@manenkov.com>
2022-09-12 11:15:28 +0300

    issues summary clustering

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
 
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from nltk.tokenize import word_tokenize
+import nltk
+
 from solution.logging import rootLogger
+from solution.train_doc2vec_summary import build_d2v_summary_model 
 from solution.load_data import \
     df_issues_train, \
     df_comment_train, \
@@ -16,6 +21,34 @@ from solution.load_data import \
     X_test, \
     y_train, \
     y_test
+
+
+build_d2v_summary_model()
+model= Doc2Vec.load("target/d2v.model")
+
+def cast_vector(row):
+    r = np.array(list(map(lambda x: x.astype('double'), row)))
+    return r
+    
+def to_vec(text):
+  test_data = word_tokenize(text.lower())
+  return cast_vector(model.infer_vector(test_data))
+
+vectors = []
+for s in merged["summary"]:
+  vectors.append(to_vec(s))
+vecs = pd.DataFrame(vectors)
+vecs.head()
+merged["summary_vectorized"] = vectors
+
+from sklearn.cluster import KMeans
+kmeans = KMeans(n_clusters=100, random_state=42) 
+kmeans.fit(vecs)
+
+res = pd.DataFrame()
+res["kmean"] = kmeans.labels_
+res['kmean'].value_counts()
+merged["summary_cluster"] = res["kmean"]
 
 
 dd = merged.groupby(['project_id'])['overall_worklogs'].agg('median').reset_index()
---
+++ solution/train_doc2vec_summary.py
@@ -0,0 +1,43 @@
+import math
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from nltk.tokenize import word_tokenize
+import nltk
+nltk.download('punkt')
+from solution.load_data import \
+    df_issues_train, \
+    df_comment_train, \
+    df_emp, \
+    df_issues_test, \
+    df_comment_test, \
+    merged
+
+
+def build_d2v_summary_model():
+  tagged_data = [TaggedDocument(words=word_tokenize(_d.lower()), tags=[str(i)]) for i, _d in enumerate(merged["summary"])]
+  max_epochs = 100
+  vec_size = 20
+  alpha = 0.025
+
+  model = Doc2Vec(vector_size=vec_size,
+                  alpha=alpha, 
+                  min_alpha=0.00025,
+                  min_count=1,
+                  dm =0)
+    
+  model.build_vocab(tagged_data)
+
+  for epoch in range(max_epochs):
+      print('iteration {0}'.format(epoch))
+      model.train(tagged_data,
+                  total_examples=model.corpus_count,
+                  epochs=model.epochs)
+      # decrease the learning rate
+      model.alpha -= 0.0002
+      # fix the learning rate, no decay
+      model.min_alpha = model.alpha
+
+  model.save("target/d2v.model")
